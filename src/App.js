652f51ace313208236f1780cab7b2c37da0344b4
arcanrun <arcan.run@gmail.com>
2018-11-09 20:47:46 +0300

    2nd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import connect from "@vkontakte/vkui-connect";
 import { View } from "@vkontakte/vkui";
@@ -40,54 +39,10 @@ class App extends React.Component {
       <View activePanel={this.state.activePanel}>
         <Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
         <Persik id="persik" go={this.go} />
+        <TestComponent />
       </View>
     );
   }
-=======
-import React from 'react';
-import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
-
-import Home from './panels/Home';
-import Persik from './panels/Persik';
-
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			activePanel: 'home',
-			fetchedUser: null,
-		};
-	}
-
-	componentDidMount() {
-		connect.subscribe((e) => {
-			switch (e.detail.type) {
-				case 'VKWebAppGetUserInfoResult':
-					this.setState({ fetchedUser: e.detail.data });
-					break;
-				default:
-					console.log(e.detail.type);
-			}
-		});
-		connect.send('VKWebAppGetUserInfo', {});
-	}
-
-	go = (e) => {
-		this.setState({ activePanel: e.currentTarget.dataset.to })
-	};
-
-	render() {
-		return (
-			<View activePanel={this.state.activePanel}>
-				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
-				<Persik id="persik" go={this.go} />
-			</View>
-		);
-	}
->>>>>>> e489a0324bf25f0d9cc9d0c4b07a4105ec887648
 }
 
 export default App;
--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import PropTypes from "prop-types";
 import {
@@ -13,20 +12,10 @@ import "./Persik.css";
 import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
 import Icon24Back from "@vkontakte/icons/dist/24/back";
 import TestComponent from "../TestComponent";
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Panel, PanelHeader, HeaderButton, platform, IOS} from '@vkontakte/vkui';
-import persik from '../img/persik.png';
-import './Persik.css';
-import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
->>>>>>> e489a0324bf25f0d9cc9d0c4b07a4105ec887648
 
 const osname = platform();
 
 const Persik = props => (
-<<<<<<< HEAD
   <Panel id={props.id}>
     <PanelHeader
       left={
@@ -45,23 +34,6 @@ const Persik = props => (
 Persik.propTypes = {
   id: PropTypes.string.isRequired,
   go: PropTypes.func.isRequired
-=======
-	<Panel id={props.id}>
-		<PanelHeader
-			left={<HeaderButton onClick={props.go} data-to="home">
-				{osname === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</HeaderButton>}
-		>
-			Persik
-		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
-	</Panel>
-);
-
-Persik.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
->>>>>>> e489a0324bf25f0d9cc9d0c4b07a4105ec887648
 };
 
 export default Persik;
